23060c713992e9485a04361e15f28dd34966d31e
paulapgar <papgar@gmail.com>
2025-03-08 00:01:18 -0500

    Add new puzzles fix formatting

--- src/index.ts
+++ src/index.ts
@@ -20,13 +20,34 @@ function setBoard(puzzleSelected:string) : Board {
                board.setCellKnown(2,7,3); board.setCellKnown(5,7,2); board.setCellKnown(8,7,8); board.setCellKnown(1,8,9); board.setCellKnown(7,8,2);
                return board;
                break;
-          case "Puzzle88":
+          case "Puzzle38":
+               board = new Board(9);
+               board.setCellKnown(1,0,1); board.setCellKnown(3,0,7); board.setCellKnown(5,0,8); board.setCellKnown(6,0,9); board.setCellKnown(0,1,3);
+               board.setCellKnown(5,1,9); board.setCellKnown(7,1,8); board.setCellKnown(4,2,5); board.setCellKnown(5,2,2); board.setCellKnown(8,2,6);
+               board.setCellKnown(0,3,8); board.setCellKnown(4,3,7); board.setCellKnown(6,3,2); board.setCellKnown(7,3,4); board.setCellKnown(8,3,9);
+               board.setCellKnown(2,4,4); board.setCellKnown(3,4,8); board.setCellKnown(4,4,9); board.setCellKnown(5,4,3); board.setCellKnown(6,4,1);
+               board.setCellKnown(0,5,9); board.setCellKnown(1,5,5); board.setCellKnown(2,5,7); board.setCellKnown(4,5,4); board.setCellKnown(8,5,3);
+               board.setCellKnown(0,6,7); board.setCellKnown(3,6,9); board.setCellKnown(4,6,2); board.setCellKnown(1,7,6); board.setCellKnown(3,7,3);
+               board.setCellKnown(8,7,2); board.setCellKnown(2,8,9); board.setCellKnown(3,8,6); board.setCellKnown(5,8,5); board.setCellKnown(7,8,7);
+               return board;
+               break;
+               case "Puzzle88":
                board = new Board(9);
                board.setCellKnown(4,0,9); board.setCellKnown(7,0,7); board.setCellKnown(2,1,8); board.setCellKnown(8,1,3); board.setCellKnown(1,2,4);
                board.setCellKnown(5,2,6); board.setCellKnown(0,3,9); board.setCellKnown(4,3,1); board.setCellKnown(6,3,6); board.setCellKnown(0,4,1);
                board.setCellKnown(3,4,5); board.setCellKnown(5,4,3); board.setCellKnown(8,4,8); board.setCellKnown(2,5,4); board.setCellKnown(4,5,7);
                board.setCellKnown(8,5,2); board.setCellKnown(3,6,7); board.setCellKnown(7,6,8); board.setCellKnown(0,7,6); board.setCellKnown(6,7,7);
                board.setCellKnown(1,8,3); board.setCellKnown(4,8,4); board.setCellKnown(5,8,8); board.setCellKnown(3,0,1);
+               return board;
+               break;
+          case "Puzzle94":
+               board = new Board(9);
+               board.setCellKnown(1,0,3); board.setCellKnown(2,0,1); board.setCellKnown(0,1,7); board.setCellKnown(2,1,8); board.setCellKnown(4,1,2);
+               board.setCellKnown(8,1,1); board.setCellKnown(3,2,8); board.setCellKnown(6,2,4); board.setCellKnown(1,3,7); board.setCellKnown(4,3,3);
+               board.setCellKnown(5,3,6); board.setCellKnown(1,4,4); board.setCellKnown(2,4,6); board.setCellKnown(6,4,2); board.setCellKnown(7,4,3);
+               board.setCellKnown(3,5,5); board.setCellKnown(4,5,7); board.setCellKnown(7,5,4); board.setCellKnown(2,6,5); board.setCellKnown(5,6,8);
+               board.setCellKnown(0,7,6); board.setCellKnown(4,7,5); board.setCellKnown(6,7,1); board.setCellKnown(8,7,2); board.setCellKnown(6,8,3);
+               board.setCellKnown(7,8,6);
                return board;
                break;
           case "Puzzle174":
